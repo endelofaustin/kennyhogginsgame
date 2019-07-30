d808f01ff1f97c11b89703343c1a2eadbd233a5e
unknown <John E>
2019-07-29 20:37:45 -0600

    use kenny face 1 image

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
+ball = pygame.image.load("kennyface1.png")
 ballrect = ball.get_rect()
 
 while 1:

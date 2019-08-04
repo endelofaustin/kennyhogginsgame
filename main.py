299857e0699a2df24f8b909e8eb53ac2143c4942
root <root@localhost.localdomain>
2019-08-03 21:51:19 -0600

    added a rectangle in white space and removed lucinda

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ ball = pygame.transform.scale(ball, ((ballrect.right - ballrect.left) * scale_fa
 lucinda  = pygame.image.load("./artwork/lucinda.png")
 ballrect = ball.get_rect()
 # move the entire rectangle encompassing Lucinda by 201 pixels horizontally and 201 pixels vertically
-lucindarect = lucinda.get_rect()
-lucindarect.left += 201
-lucindarect.right += 201
-lucindarect.top += 201
-lucindarect.bottom += 201
+##lucindarect = lucinda.get_rect()
+#lucindarect.left += 201
+#lucindarect.right += 201
+#lucindarect.top += 201
+#lucindarect.bottom += 201
 
 # this is the main game loop!
 while 1:
@@ -57,7 +57,7 @@ while 1:
         ballrect = ballrect.move([-speed[0], 0])
     if ballrect.top < 0 or ballrect.bottom > height:
         ballrect = ballrect.move([0, -speed[1]])
-
+    
     # this is commented out so that we can test only kenny's movement for now
     #lucindarect = lucindarect.move(speed)
     #if lucindarect.left < 0 or lucindarect.right > width:
@@ -65,8 +65,38 @@ while 1:
     #if lucindarect.top < 0 or lucindarect.bottom > height:
     #    speed[1] = -speed[1]
 
-    screen.fill(gray)
+    # Creation of a basic platform that Kenny can jump onto 
+    # A nested list 
+
+    platform = [ 
+         #  0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+           [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+           [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0], ]
+
+    # iterate through the nested list and render a rectangle if a 1 is in that position
+    
+    DISPLAY = pygame.display.set_mode((800,600), 0,32)
+    WHITE = (255, 255, 255,)
+    GREEN = (0, 255, 0,)
+    DISPLAY.fill(WHITE)
+
+    for row in platform:
+        for item in row:
+            if item == 0:
+                pass
+            elif item == 1:
+                # arguments for rect placements are (x axis, y axis, height width
+                pygame.draw.rect(DISPLAY, GREEN, (120,500,350,100))
+
+            
+#   screen.fill(gray)
     screen.blit(ball, ballrect)
-    screen.blit(lucinda, lucindarect)
+#    screen.blit(lucinda, lucindarect)
     screen.blit(textsurface, (0,0))
     pygame.display.flip()

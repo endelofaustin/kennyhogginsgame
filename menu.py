5130e1b4ad6b7963b4800657c6139f156d751c89
Austin Endel <53027219+endelofaustin@users.noreply.github.com>
2023-10-17 16:38:30 -0700

    Updating the menu to include backgroud image for more fun

--- menu.py
+++ menu.py
@@ -1,24 +1,29 @@
 import pyglet
 from engineglobals import EngineGlobals
+import pyglet.resource, pyglet.image
+from pyglet.sprite import Sprite
 
 class GameMenu():
 
     def __init__(self):
 
+        # Load the background image
+        self.background_image = pyglet.image.load('artwork/StartItUp.png')
         self.menu_batch = pyglet.graphics.Batch()
-
+        self.sprite = Sprite(img=self.background_image, batch=self.menu_batch)
         self.label_style = {
             'font_name': 'Arial',
             'font_size': 36,
             'bold': True,
             'color': (255, 255, 255, 255)
         }
+        
 
-        self.start_label = pyglet.text.Label('Start', x=400, y=400, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
-        self.settings_label = pyglet.text.Label('Settings', x=400, y=300, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
-        self.password_label = pyglet.text.Label('Password', x=400, y=200, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
+        self.start_label = pyglet.text.Label('Start', x=550, y=400, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
+        self.settings_label = pyglet.text.Label('Settings', x=550, y=300, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
+        self.password_label = pyglet.text.Label('Password', x=550, y=200, anchor_x='center', anchor_y='center', batch=self.menu_batch, **self.label_style)
 
-        self.password = ""
+        self.password = "          "
         self.password_label_visible = False
 
     def on_draw(self):
@@ -43,13 +48,12 @@ class GameMenu():
                  self.settings_label.y - self.settings_label.content_height / 2 <= y <= self.settings_label.y + self.settings_label.content_height / 2:
                 # Handle "Settings" button click
                 print("Settings button clicked")
-                self.menu_batch.add(pyglet.shapes.Rectangle(0, 0, 800, 600, color=(0, 0, 255)))
 
             elif self.password_label.x - self.password_label.content_width / 2 <= x <= self.password_label.x + self.password_label.content_width / 2 and \
                  self.password_label.y - self.password_label.content_height / 2 <= y <= self.password_label.y + self.password_label.content_height / 2:
                 # Handle "Password" button click
-                print("Password button clicked")
                 self.password_label_visible = True
+                print("Password Clicked")
 
     def handle_text_input(self, text):
 
